6273379b5e55d742508cf1f10232fe83bc8b853f
Ben Artzi Shachar <gal.ben.artzi.shachar@intel.com>
2022-10-21 23:52:11 +0300

    create game and logic classes

---
+++ src/Game.java
@@ -0,0 +1,7 @@
+import javax.swing.JOptionPane;
+
+public class Game {
+    public static void main(String args []){
+        JOptionPane.showMessageDialog(null, "Welcome to Cows And Bulls\n Enter your guess");
+    }
+}
